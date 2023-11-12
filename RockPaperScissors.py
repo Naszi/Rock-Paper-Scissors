57eb34c10a608a88934cc876476045a77c3bc35c
Nagy Szabolcs <szabolcs.nagy@nerddevelopments.com>
2023-11-12 22:33:21 +0100

    add choose you and computer random

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -25,4 +25,11 @@ scissors = '''
 ---.__(___)
 '''
 
-print(paper)
+import random
+
+rock_paper_scissors = [rock, paper, scissors]
+your_choose = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for  Scissors.\n"))
+print(rock_paper_scissors[your_choose])
+computer_choose = random.randint(0, 2)
+print("Computer chose:")
+print(rock_paper_scissors[computer_choose])
